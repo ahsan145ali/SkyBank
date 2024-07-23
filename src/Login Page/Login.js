2a913430290661fddfea668792e76b0f2fcfa30b
Ahsan Ali <ahsan145ali@gmail.com>
2024-07-23 16:57:56 +0100

    added html css and js for login form with animations

--- src/Login Page/Login.js
+++ src/Login Page/Login.js
@@ -1,8 +1,74 @@
 import React from 'react'
-
+import './Login.css';
+import { useState } from 'react';
 const Login = () => {
+
+    const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsSignUp(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUp(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+  };
   return (
-    <div>Login</div>
+    <>
+    <div className={`container ${isSignUp ? 'active' : ''}`} class="container" id="container">
+        <div class="form-container sign-up">
+            <form onSubmit={handleSubmit} >
+                <h1>Create Account</h1>
+                <div class="social-icons">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fa-brands fa-google"></i></a>
+                    <a href="#" class="social"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email for registration</span>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form onSubmit={handleSubmit}>
+                <h1>Sign In</h1>
+                <div class="social-icons">
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fa-brands fa-google"></i></a>
+                    <a href="#" class="social"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email password</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login" onClick={handleSignInClick}>Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Register with your personal details to use all of site features</p>
+                    <button class="hidden" id="register" onClick={handleSignUpClick}>Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    </>
   )
 }
 
