e1930c36f2c9a12fa1ea7304502bc1430a1a46d1
Ahsan Ali <ahsan145ali@gmail.com>
2024-07-26 17:24:33 +0100

    Changed parameter name in StoreDB

--- src/LoginPage/HelperFunctions/StoreinDB.js
+++ src/LoginPage/HelperFunctions/StoreinDB.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import {sha256} from 'js-sha256';
-const baseCustomerUrl = "http://localhost:4001/Customers";
+const baseCustomerUrl = "http://localhost:4000/Customers";
 
-export const SendUserToDatabase = async({customerName,customerEmail,customerPassword})=>{ 
-   customerPassword = getPasswordHash(customerPassword); 
-  await axios.post(baseCustomerUrl,{customerName,customerEmail,customerPassword}).then(()=>{
+export const SendUserToDatabase = async(customer)=>{ 
+   customer.customerPassword = getPasswordHash(customer.customerPassword); 
+  await axios.post(baseCustomerUrl,customer).then(()=>{
     console.log("Posted to database");
   }).catch((e)=>{
     console.log(`There was a problem adding: ${e.message}`);
