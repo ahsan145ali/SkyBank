20902e0222e3a138ed3fdb01130b90c34f1b16b5
ehallam <elliothallam@gmail.com>
2024-08-06 11:50:39 +0100

    Things are looking better

--- src/Homepage/Service.jsx
+++ src/Homepage/Service.jsx
@@ -6,16 +6,16 @@ const Service = (props) => {
     return (
         <>
             <tr>
-                <td><h3 style={{color: "white"}}>Current Accounts</h3><br/>Find an account that works for you</td>
-                <td><h3 style={{color: "white"}}>Investments</h3><br/>Make the most of your money</td>
-                <td><h3 style={{color: "white"}}>Investments</h3><br/>Make the most of your money</td>
-                <td><h3 style={{color: "white"}}>Investments</h3><br/>Make the most of your money</td>
+                <td><h3 style={{color: "black"}}>Current Accounts</h3><br/>Find an account that works for you</td>
+                <td><h3 style={{color: "black"}}>Investments</h3><br/>Make the most of your money</td>
+                <td><h3 style={{color: "black"}}>Investments</h3><br/>Make the most of your money</td>
+                <td><h3 style={{color: "black"}}>Investments</h3><br/>Make the most of your money</td>
             </tr>
             <tr>
-                <td><h3 style={{color: "white"}}>Current Accounts</h3><br/>Find an account that works for you</td>
-                <td><h3 style={{color: "white"}}>Investments</h3><br/>Make the most of your money</td>
-                <td><h3 style={{color: "white"}}>Investments</h3><br/>Make the most of your money</td>
-                <td><h3 style={{color: "white"}}>Investments</h3><br/>Make the most of your money</td>
+                <td><h3 style={{color: "black"}}>Current Accounts</h3><br/>Find an account that works for you</td>
+                <td><h3 style={{color: "black"}}>Investments</h3><br/>Make the most of your money</td>
+                <td><h3 style={{color: "black"}}>Investments</h3><br/>Make the most of your money</td>
+                <td><h3 style={{color: "black"}}>Investments</h3><br/>Make the most of your money</td>
             </tr>
         </>
     )
