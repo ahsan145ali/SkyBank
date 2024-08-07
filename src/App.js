6cd97b1ff221bbd782c97999a153dd3907cefcdc
ehallam <elliothallam@gmail.com>
2024-08-07 16:49:14 +0100

    merged

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Context/AuthContext';
-
 import { PayeeProvider } from './Components/TransactionPage/PayeeContext'; // Adjust the path
 import Header from './Components/TransactionPage/Header';
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2ca1f0dc98a5c589e1a412c04f15b78bded2562f
 import PayeeList from './Components/TransactionPage/PayeeList';
 import AddPayee from './Components/TransactionPage/AddPayee';
 import PayPayee from './Components/TransactionPage/PayPayee';
 import Navbar from './Navbar/Navbar.js';
 import Dashboard from './Dashboard/Dashboard.jsx';
-import TransactionHistory from './TransactionHistory/TransactionHistory.jsx'
+import TransactionHistory from './Transactions/TransactionHistory.jsx'
 import Footer from './Footer/Footer.js';
 import Homepage from './Homepage/Homepage.jsx';
 import Login from './LoginPage/Login.js';
@@ -21,9 +23,9 @@ import Login from './LoginPage/Login.js';
 function App() {
   return (
       <Router>
-        {/* <Navbar/> */}
+        <Navbar/>
         <AuthProvider>
-
+          
           <PayeeProvider>
             {/* <Header /> */}
             <Routes>
