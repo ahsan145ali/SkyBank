c7b5f79e7e1ba047de8b655a0cc5a3f3fc97c85e
Alan Jack <aldojack@hotmail.co.uk>
2024-08-13 09:52:53 +0100

    Transactions now load properly

--- src/Transactions/TransactionContainer.jsx
+++ src/Transactions/TransactionContainer.jsx
@@ -7,19 +7,25 @@ import axios from 'axios';
 function TransactionContainer() {
     const {userDetails} = useUserContext();
     const [transactions, setTransactions] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    let renderTransactions;
 
     useEffect(() => {
         axios.get(`http://localhost:8081/transaction/getAll/${userDetails.email}`)
         .then(({data}) => {
             console.log(data);
             setTransactions(data)
+            setIsLoading(false);
         })
     },[])
-    const renderTransactions = transactions.map(({transactionDate, description, amountIn, amountOut, id}) => {
+
+    renderTransactions = transactions?.map(({transactionDate, description, amountIn, amountOut, id}) => {
         return (
             <TransactionItem transactionDate={transactionDate} description={description} amountIn={amountIn} amountOut={amountOut} key={id}/>
         )
     })
+
     return (
         <section className='tw-mx-6 tw-p-4 tw-shadow-md'>
             <div className='tw-grid tw-grid-cols-12'>
@@ -32,7 +38,7 @@ function TransactionContainer() {
                     <TransactionItem date="15 July" description="Sky TV" outAmount={"75.00"} balance={"20200.00"}/>
                     <TransactionItem date="1 July" description="British Gas" outAmount={"100.00"} balance={"2275.00"}/>
                     <TransactionItem date="1 July" description="Wage" inAmount={"2275.00"} balance={"0.00"}/> */}
-                    {renderTransactions}
+                    {!isLoading ? renderTransactions : <p>Loading...</p>}
                 </div>
             </div>
         </section>
