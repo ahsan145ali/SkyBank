4b3aef814da67b65c3d773ec2ff1dd1baadf26e8
Alan Jack <aldojack@hotmail.co.uk>
2024-08-13 15:31:31 +0100

    updated private route to use userContext instead of AuthContext

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../Context/AuthContext';
+import { useUserContext } from '../Context/UserContext';
 
 const PrivateRoute = () => {
   const { contextToken } = useAuth();
-  return contextToken ? <Outlet/> : <Navigate to="/" />;
+  const {userDetails} = useUserContext();
+  return userDetails ? <Outlet/> : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
